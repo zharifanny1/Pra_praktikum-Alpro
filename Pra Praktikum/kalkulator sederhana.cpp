3d2c9a9415a817707bb93c8411271d2aff629d35
Arya Shiraath Barada <arya2100018001@webmail.uad.ac.id>
2022-03-21 10:38:52 +0700

    Simply Design

--- Pra Praktikum/kalkulator sederhana.cpp
+++ Pra Praktikum/kalkulator sederhana.cpp
@@ -28,56 +28,75 @@ int main()
 	int a, b, d, e, g, h;
 	float j, k;
 	char menu;
-	cout << "PROGRAM KALKULATOR" << endl;
-	cout << "Menu Operasi:" << endl;
-	cout << " 1. Penambahan" << endl;
-	cout << " 2. Pengurangan" << endl;
-	cout << " 3. Perkalian" << endl;
-	cout << " 4. Pembagian" << endl;
+	cout << "======================" <<endl;
+	cout << "| PROGRAM KALKULATOR |" << endl;
+	cout << "======================" <<endl;
+	cout << "|  Menu Operasi :    |" << endl;
+	cout << "| 1. Penambahan      |" << endl;
+	cout << "| 2. Pengurangan     |" << endl;
+	cout << "| 3. Perkalian       |" << endl;
+	cout << "| 4. Pembagian       |" << endl;
+	cout << "======================" <<endl;
 
-	cout << "Pilih Menu No: ";
+	cout << "Pilih Menu Nomor : ";
 	cin >> menu;
 
 	switch (menu)
 	{
 	case '1':
 	{
-		cout << "Bilangan Pertama: ";
+		cout << "======================" <<endl;
+		cout << "|     Penambahan     |" <<endl;
+		cout << "======================" <<endl;
+		cout << "Bilangan Pertama : ";
 		cin >> a;
-		cout << "Bilangan Kedua: ";
+		cout << "Bilangan Kedua   : ";
 		cin >> b;
 		int hasil1 = penambahan(a, b);
-		cout << "Hasil " << a << " + " << b << " = " << hasil1;
+		cout << "Hasil dari " << a << " + " << b << " = " << hasil1 <<endl;
+		cout << "======================" <<endl;
 		break;
 	}
 	case '2':
 	{
-		cout << "Bilangan Pertama: ";
+		cout << "======================" <<endl;
+		cout << "|    Pengurangan     |" <<endl;
+		cout << "======================" <<endl;
+		cout << "Bilangan Pertama : ";
 		cin >> d;
-		cout << "Bilangan Kedua: ";
+		cout << "Bilangan Kedua   : ";
 		cin >> e;
 		int hasil2 = pengurangan(d, e);
-		cout << "Hasil " << d << " - " << e << " = " << hasil2;
+		cout << "Hasil dari " << d << " - " << e << " = " << hasil2 <<endl;
+		cout << "======================" <<endl;
 		break;
 	}
 	case '3':
 	{
-		cout << "Bilangan Pertama: ";
+		cout << "======================" <<endl;
+		cout << "|     Perkalian      |" <<endl;
+		cout << "======================" <<endl;
+		cout << "Bilangan Pertama : ";
 		cin >> g;
-		cout << "Bilangan Kedua: ";
+		cout << "Bilangan Kedua   : ";
 		cin >> h;
 		int hasil3 = perkalian(g, h);
-		cout << "Hasil " << g << " * " << h << " = " << hasil3;
+		cout << "Hasil dari " << g << " * " << h << " = " << hasil3 <<endl;
+		cout << "======================" <<endl;
 		break;
 	}
 	case '4':
 	{
-		cout << "Bilangan Pertama: ";
+		cout << "======================" <<endl;
+		cout << "|     Pembagian      |" <<endl;
+		cout << "======================" <<endl;
+		cout << "Bilangan Pertama : ";
 		cin >> j;
-		cout << "Bilangan Kedua: ";
+		cout << "Bilangan Kedua   : ";
 		cin >> k;
 		float hasil4 = pembagian(j, k);
-		cout << "Hasil " << j << " / " << k << " = " << hasil4;
+		cout << "Hasil dari " << j << " / " << k << " = " << hasil4 <<endl;
+		cout << "======================" <<endl;
 		break;
 	}
 	default:
